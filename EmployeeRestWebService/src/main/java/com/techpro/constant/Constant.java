f71deb28fbf208d3cc04939ccdfa6bdf857fda0a
ganeshnikat <ganesh.nikat@gmail.com>
2018-11-26 10:47:45 +0100

    Update Constant.java

--- EmployeeRestWebService/src/main/java/com/techpro/constant/Constant.java
+++ EmployeeRestWebService/src/main/java/com/techpro/constant/Constant.java
@@ -8,8 +8,8 @@ public class Constant {
 	/*------Oracle Database Details START------*/
 	public static final String DB_DRIVER_CLASS_NAME = "oracle.jdbc.driver.OracleDriver";
 	public static final String DB_URL = "jdbc:oracle:thin:@127.0.0.1:1521:xe";
-	public static final String DB_USERNAME = "NTT";
-	public static final String DB_PASSWORD = "NTTDATA";
+	public static final String DB_USERNAME = "apache";
+	public static final String DB_PASSWORD = "camel";
 	/*------Oracle Database Details END------*/
 	public static final String SELECT_ALL_EMPLOYEE_QUERY = "SELECT * FROM EMPLOYEE";
 	public static final String ADD_EMPLOYEE_QUERY = "INSERT INTO EMPLOYEE (EMPLOYEE_ID, EMPLOYEE_NAME, CITY) VALUES(?,?,?)";
